3c27490d1a20c0c121e34d10384efc5e2d4bc56c
Natalia <nika_pozitivka@mail.ru>
2022-04-29 12:10:53 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int score = 100;
+        int payment = 1100;
+        int bonus;
+
+        if (payment >= 1000) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
+        }
+        int balance = score + payment + bonus;
+
+        System.out.println("На вашем счету: " + balance + "руб.");
+        System.out.println("Бонус: " + bonus + "руб.");
+    }
+}
